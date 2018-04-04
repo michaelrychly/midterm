a898670574f936681204d921ef1a7187353cd786
Nucleartom <jacob.maarse@gmail.com>
2018-04-04 22:03:50 +0000

    Adding styles from bootstrap and some basic edits that will be the base for our page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,16 @@
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
-});
+(function($) {
+  "use strict"; // Start of use strict
+   
+ // Modal popup$(function () {
+  $('.portfolio-item').magnificPopup({
+    type: 'inline',
+    preloader: false,
+    focus: '#username',
+    modal: true
+  });
+  $(document).on('click', '.portfolio-modal-dismiss', function(e) {
+    e.preventDefault();
+    $.magnificPopup.close();
+  });
+
+})(jQuery); // End of use strict
