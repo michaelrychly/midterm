411e3bb3d4410fb625c4bcd353f84351140db99c
Nucleartom <jacob.maarse@gmail.com>
2018-04-05 00:48:06 +0000

    created drop down list of items, in theory for selected list

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,16 +1,29 @@
-(function($) {
-  "use strict"; // Start of use strict
+"use strict"; // Start of use strict
 
- // Modal popup$(function () {
+$(document).ready(function () {
+  // Modal popup$(function () {
   $('.portfolio-item').magnificPopup({
     type: 'inline',
     preloader: false,
     focus: '#username',
     modal: true
   });
-  $(document).on('click', '.portfolio-modal-dismiss', function(e) {
+  $(document).on('click', '.portfolio-modal-dismiss', function (e) {
     e.preventDefault();
     $.magnificPopup.close();
   });
+  $('#list-title-bar').on('click', function () {
+    slideList()
+  });
 
-})(jQuery); // End of use strict
+}); // End of use strict
+
+let shown = true;
+function slideList() {
+  $("#list-of-items").slideToggle("slow")
+  if (shown === true) {
+    shown = false;
+  } else {
+    shown = true;
+  }
+}
