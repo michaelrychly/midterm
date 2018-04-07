3b6566dd96a3efa0643d62961bd64191a2769776
Nucleartom <jacob.maarse@gmail.com>
2018-04-07 15:11:20 +0000

    made some minor adjustments to logout and forced links to create new tab

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -72,59 +72,74 @@ $(document).ready(function (e) {
     $('#id01').find('button.register').on('click', function (e) {
       e.preventDefault();
       e.stopImmediatePropagation();
-      register();
+      register().then(() => {
+        $('#logoutBtn').on('click', function (e) {
+          location.reload();
+        })
+      })
     });
     $('#id01').find('button.login').on('click', function (e) {
       e.preventDefault();
       e.stopImmediatePropagation();
-      login();
+      login().then(() => {
+        $('#logoutBtn').on('click', function (e) {
+          location.reload();
+        })
+      })
     });
   })
 });
 
 function login() {
-  if ($('form').find('#username-field').val() === "" || $('form').find('#password-field').val() === "") {
-    alert("Cannot Login/Register with an empty item");
-  } else {
-    $.ajax({
-      url: '/api/users/login',
-      method: 'PUT',
-      data: { username: $('form').find('#username-field').val(), password: $('form').find('#password-field').val() },
-      success: ((res) => {
-        if (!res[1]) {
-          alert('Unacceptable Username/Password')
-        } else {
-          // location = location;
-          removeNavBar();
-          $('#id01').css('display', '');
-          loadNavBar(res[0]);
-        }
+  return new Promise(function (resolve, reject) {
+    if ($('form').find('#username-field').val() === "" || $('form').find('#password-field').val() === "") {
+      alert("Cannot Login/Register with an empty item");
+    } else {
+      $.ajax({
+        url: '/api/users/login',
+        method: 'PUT',
+        data: { username: $('form').find('#username-field').val(), password: $('form').find('#password-field').val() },
+        success: ((res) => {
+          if (!res[1]) {
+            alert('Unacceptable Username/Password')
+            return reject();
+          } else {
+            // location = location;
+            removeNavBar();
+            $('#id01').css('display', '');
+            loadNavBar(res[0]);
+            return resolve();
+          }
+        })
       })
-    })
-  }
+    }
+  })
 }
 
 function register() {
-  if ($('form').find('#username-field').val() === "" || $('form').find('#password-field').val() === "") {
-    alert("Cannot Login/Register with an empty item");
-  } else {
-    $.ajax({
-      url: '/api/users/register',
-      method: 'PUT',
-      data: { username: $('form').find('#username-field').val(), password: $('form').find('#password-field').val() },
-      success: ((res) => {
-        if (!res[1]) {
-          alert('Unacceptable Username/Password')
-        } else {
-          //location = location;
-          removeNavBar();
-          $('#id01').css('display', '');
-          loadNavBar(res[0]);
-
-        }
+  return new Promise(function (resolve, reject) {
+    if ($('form').find('#username-field').val() === "" || $('form').find('#password-field').val() === "") {
+      alert("Cannot Login/Register with an empty item");
+    } else {
+      $.ajax({
+        url: '/api/users/register',
+        method: 'PUT',
+        data: { username: $('form').find('#username-field').val(), password: $('form').find('#password-field').val() },
+        success: ((res) => {
+          if (!res[1]) {
+            alert('Unacceptable Username/Password')
+            return reject();
+          } else {
+            //location = location;
+            removeNavBar();
+            $('#id01').css('display', '');
+            loadNavBar(res[0]);
+            return resolve();
+          }
+        })
       })
-    })
-  }
+    }
+  })
 }
 
 function removeNavBar() {
@@ -135,7 +150,7 @@ function loadNavBar(username) {
     let output = `<li class="nav-item mx-0 mx-lg-1">
           <a id="username" class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger">${username}</a>
         </li><li class="nav-item mx-0 mx-lg-1">
-        <a id="logoutBtn" class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger">Logout</a>
+        <a id="logoutBtn" class="logoutBtn nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger">Logout</a>
       </li>`;
     $('nav').find('ul').append(output)
   } else {
@@ -191,7 +206,6 @@ function deleteItem(id) {
 }
 
 function updateItemText(id) {
-  console.log(id);
   let output = $('form').find('#item-update-field').val();
   if ($('form').find('#item-update-field').val() === "") {
     output = $('#id02').find('input').attr("placeholder");
@@ -200,26 +214,21 @@ function updateItemText(id) {
   if ($('form').find('#item-update-field').val() === "" && $('form').find('#catDropDown').val() == 0) {
     alert("Cannot update item with no new info");
   } else {
-    console.log('form val: ', $('form').find('#item-update-field').val())
-    console.log('should be expected new text: ', output);
-    console.log($('form').find('#catDropDown').val())
     $.ajax({
       url: `/api/items/${id}`,
       method: 'PUT',
       data: { text_from_user: output, category_id: $('form').find('#catDropDown').val() ? $('form').find('#catDropDown').val() : 0 }
     }).done(() => {
       if ($('form').find('#item-update-field').val() === "") {
-        console.log('in empty text')
         $(`li#${id}`).remove();
       } else if ($('form').find('#catDropDown').val() == 0) {
         let content = $(`li#${id}`).contents()
         content[0] = output;
         $(`li#${id}`).contents().remove();
-        for (let i = 0; i < content.length; i ++) {
+        for (let i = 0; i < content.length; i++) {
           $(`li#${id}`).append(content[i]);
         }
       } else {
-        console.log('in else')
         $(`li#${id}`).val($('form').find('#item-update-field').val())
         $(`li#${id}`).remove();
       }
@@ -237,10 +246,7 @@ function updateItemState(id) {
     method: 'PUT',
     data: { state: 'would you please kindly toggle the state' },
     success: ((state) => {
-      console.log('in success')
       //toggle change font awesome color to green or off green depending on server response
-      console.log(id)
-      console.log($(`li#${id}`))
       $(`li#${id}`).find('.fa-check-square-o').removeClass('green')
       if (state) {
         $(`li#${id}`).find('.fa-check-square-o').addClass('green')
@@ -295,7 +301,7 @@ function createItem(item, list, cat) {
     let query = str.split(' ').join('+');
     suggestedURL = externalLinks[externalLinks.length - 1].split(' ').join(query);
   }
-  let output = `<li id="${item.id}">${item.text_from_user}<i class="${item.state ? "green" : ""} modifyItem fa fa-check-square-o"></i><i id="item.id" class="modifyItem fa fa-edit"></i><i id="item.id" class="modifyItem fa fa-minus-square-o"></i><a class="modifyItem fa fa-link" href=${suggestedURL}></a></li>`
+  let output = `<li id="${item.id}">${item.text_from_user}<i class="${item.state ? "green" : ""} modifyItem fa fa-check-square-o"></i><i id="item.id" class="modifyItem fa fa-edit"></i><i id="item.id" class="modifyItem fa fa-minus-square-o"></i><a class="modifyItem fa fa-link" href=${suggestedURL} target="_blank"></a></li>`
   $(`#${list}`).prepend(output);
 }
 
